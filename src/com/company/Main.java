372861cfd0854fe12b09aace8853243f29c76c6b
sergey <sergejturlanov207@gmail.com>
2021-03-27 10:59:22 +0300

    'done'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Triangle triangle = new Triangle();
+        triangle.print();
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ведите стороны треугольника");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        Triangle triangle1 = new Triangle(a, b);
+        triangle1.print();
+
     }
 }
--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -1,4 +1,38 @@
 package com.company;
 
 public class Triangle {
+    private final int a, b;
+
+    Triangle(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    Triangle() {
+        a = 5;
+        b = 7;
+    }
+
+    public int Hypotenuse() {
+        return (int) Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+
+    public int Square() {
+        return a * b;
+    }
+
+    public void print() {
+        System.out.println("первая сторона = " + a + "\n" +
+                "вторая сторона = " + b + "\n" + "третья сторона = " +
+                Hypotenuse());
+
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
 }
